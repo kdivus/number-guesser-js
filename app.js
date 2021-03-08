2a87b3db445019ef11598e699d766ead89ed6a3c
Krešimir Divić <kdivus@hotmail.com>
2021-03-08 13:46:18 +0100

    finishing up js

--- app.js
+++ app.js
@@ -9,11 +9,11 @@
 // game values
 let min = 1,
     max = 10,
-    winningNum = 2,
+    winningNum = getRandomNum(min, max),
     guessesLeft = 3;
 
 // UI elements
-const game = document.querySelector('game'),
+const game = document.querySelector('#game'),
       minNum = document.querySelector('.min-num'),    
       maxNum = document.querySelector('.max-num'),    
       guessBtn = document.querySelector('#guess-btn'),    
@@ -22,7 +22,16 @@ const game = document.querySelector('game'),
       
 // assing UI min and max
 minNum.textContent = min;
-maxNum.textContent = max;    
+maxNum.textContent = max;  
+
+
+//play again event listener
+game.addEventListener('mousedown', function(e){
+    if(e.target.className === 'play-again'){
+        window.location.reload();
+    }
+});
+
 
 // listen for guess
 guessBtn.addEventListener('click', function(){
@@ -53,8 +62,7 @@ guessBtn.addEventListener('click', function(){
             // tell user ikts the wrong number
             setMessage(`${guess} is not correct, ${guessesLeft} guesses left.`, 'red');
         }
-    }
-    
+    }  
 })
 
 // set message function
@@ -77,4 +85,13 @@ function gameOver(won, msg) {
     message.style.color = color;
     //set message
     setMessage(msg);
+
+    //play again?
+    guessBtn.value = 'Play again';
+    guessBtn.className += 'play-again';
+}
+
+// get winnig number
+function getRandomNum(min, max){
+    return Math.floor(Math.random() * (max-min+1) + min);
 }
